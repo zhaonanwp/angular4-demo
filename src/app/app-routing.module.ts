4d9aa20e186f974b869dcfc69ff08524b607b9d7
zhaowp <zn2012@gmail.com>
2017-06-05 14:40:25 +0800

    add index home

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import {NgModule} from '@angular/core';
+import {RouterModule,Routes} from '@angular/router';
+
+import {HomeComponent} from './home/app.home';
+
+
+const appRoutes:Routes=[
+    {path:'home',component:HomeComponent},
+    {path:'',redirectTo:'/home',pathMatch:'full'}
+];
+
+@NgModule({
+    imports:[
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports:[
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule{
+    
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule} from './app-routing.module'; 
+
+import {HomeComponent} from './home/app.home';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
